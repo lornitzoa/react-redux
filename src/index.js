267c01f47381a01590fc6de26faf3a22952705bd
Anna Lornitzo <lornitzoa@gmail.com>
2019-05-10 15:36:09 -0400

    updated Readme.md

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux'
+import counterReducer from './reducer'
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(counterReducer)
+
+console.log(store);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
