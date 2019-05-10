bfca445d51cc8c83664f8dced1de244536a3c1f2
Anna Lornitzo <lornitzoa@gmail.com>
2019-05-10 15:48:47 -0400

    completed redux beginners tutorial

--- src/index.js
+++ src/index.js
@@ -6,11 +6,35 @@ import { createStore } from 'redux'
 import counterReducer from './reducer'
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(counterReducer)
+const store = createStore(counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-console.log(store);
+const increment = () => {
+  store.dispatch({type: 'INCREMENT'})
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const decrement = () => {
+  store.dispatch({type: 'DECREMENT'})
+}
+
+const Counter = () => {
+  return (
+    <div>
+      <h1>{store.getState()}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  )
+}
+
+
+
+
+const render = () => ReactDOM.render(<Counter />, document.getElementById('root'));
+
+render()
+store.subscribe(render)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
